95e42d4c4da4a0f5646f033751ea7993118df5a1
Peter Donker <peter@bring2mind.net>
2015-11-28 12:14:14 +0100

    Ability to specify year in querystring

--- Controls/RankingsTable.ascx.vb
+++ Controls/RankingsTable.ascx.vb
@@ -4,16 +4,18 @@
 
   Public Property CategoryId As Integer = -1
   Public Property NrRecords As Integer = 0
+  Public Property Year As Integer = DateTime.Now.Year
 
   Private Sub Page_Init(sender As Object, e As System.EventArgs) Handles Me.Init
    LocalResourceFile = Common.Globals.glbSharedResources
+   Me.Request.Params.ReadValue("Year", Year)
   End Sub
 
   Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
    If Not Me.IsPostBack Then
 
-    Using ir As IDataReader = Data.DataProvider.Instance.GetDistancesStandings(ModuleId, Now.Year, CategoryId)
+    Using ir As IDataReader = Data.DataProvider.Instance.GetDistancesStandings(ModuleId, Year, CategoryId)
      rpRankingsPara.DataSource = ir
      rpRankingsPara.DataBind()
     End Using
